98803f5a92768a57d7779cbe55acda80ed522362
洪鹭燕 <luyanhong213010@sohu-inc.com>
2018-11-21 18:09:09 +0800

    添加 polyfill 添加播放器  进度条  时间计时器

--- src/components/PlayBox/index.jsx
+++ src/components/PlayBox/index.jsx
@@ -11,6 +11,7 @@ const READY = 0;
 const PLAYING = 1;
 const PAUSE = 2;
 let index = 0;
+let progress;
 class PlayBox extends Component {
   static propTypes = {
     playListId: PropTypes.number.isRequired
@@ -23,7 +24,9 @@ class PlayBox extends Component {
       picUrl: '',
       playlist: null,
       playState: READY,
-      duration: 0
+      duration: 0,
+      playProgress: 0,
+      timeProgress: '00:00'
     };
     this.myRef = React.createRef();
     this.onPlay = this.onPlay.bind(this);
@@ -43,29 +46,51 @@ class PlayBox extends Component {
       return;
     }
     this.myRef.current.play();
+    this.onProgress();
     this.setState({
       playState: PLAYING
     });
   }
 
+  // 进度条渲染
+  onProgress () {
+    if (progress) {
+      clearInterval(progress);
+    }
+    const { playProgress, duration } = this.state;
+    progress = setInterval(() => {
+      // 暂停
+      const { playState } = this.state;
+      if (playState !== PLAYING) {
+        clearInterval(progress);
+      }
+      // 时间到了停止
+      if (playProgress >= duration) {
+        clearInterval(progress);
+      }
+      this.setState((prevState) => ({
+        playProgress: prevState.playProgress + 1,
+        timeProgress: this.timePretty(prevState.playProgress + 1)
+      }));
+    }, 1000);
+  }
+
   /**
-   * 获取音乐
+   * 获取音乐, 并开始播放
    * @param {Number}  音乐id
   */
-  fetchData (id) {
+  async fetchData (id) {
     try {
-      getMusic(id).then((res) => {
-        if (res.code === 200 && res.data[0].url) {
-          const audio = this.myRef.current;
-          audio.src = res.data[0].url;
-          this.onPlay();
-          audio.addEventListener('ended', () => {
-            this.next();
-          }, false);
-        }
-      }).catch((err) => {
-        console.log(err);
-      });
+      const res = await getMusic(id);
+      if (res.code === 200 && res.data[0].url) {
+        const audio = this.myRef.current;
+        audio.src = res.data[0].url;
+        this.onPlay();
+        this.onProgress();
+        audio.addEventListener('ended', () => {
+          this.next();
+        }, false);
+      }
     } catch (err) {
       console.log(err);
     }
@@ -91,11 +116,13 @@ class PlayBox extends Component {
     const play = this.state.playlist.tracks[index];
     const { name } = play;
     const { picUrl } = play.al;
-    const duration = (play.l.size * 8) / play.l.br;
+    const duration = parseInt((play.l.size * 8) / play.l.br, 10);
     this.setState({
       name,
       picUrl,
-      duration
+      duration,
+      playProgress: 0,
+      timeProgress: '00:00'
     });
     this.fetchData(play.id);
   }
@@ -148,10 +175,13 @@ class PlayBox extends Component {
       name,
       picUrl,
       playState,
-      duration
+      duration,
+      playProgress,
+      timeProgress
     } = this.state;
     const isPlaying = playState === PLAYING;
     const durationPretty = this.timePretty(duration);
+    const leftPx = Math.trunc(500 * playProgress / duration);
     return (
       <div className="play">
         <div className="play-content">
@@ -162,6 +192,15 @@ class PlayBox extends Component {
               <i className={`iconfont icon-pause pause ${!isPlaying ? 'hide' : ''}`} onClick={this.pause}></i>
             </div>
             <div><i className="iconfont icon-next" onClick={this.next.bind(this)}></i></div>
+            <div className="play-action-volumn">
+              <i className="iconfont icon-volume"></i>
+              <div className="volumn-box">
+                <div className="volumn-box-progress">
+                  <progress className="volumn-progress" value="4" max="10"></progress>
+                  <div className="volumn-progress-btn"></div>
+                </div>
+              </div>
+            </div>
           </div>
           <div className="play-info">
             <div className="play-info-pic">
@@ -170,13 +209,11 @@ class PlayBox extends Component {
             <div className="play-info-box">
               <h4>{name}</h4>
               <div className="play-progress">
-                {/* <div className="play-progress-bar"></div> */}
-                <progress value="0" max={duration}></progress>
-                <div className="play-progress-btn"></div>
+                <progress className="music-progress" value={playProgress} max={duration}></progress>
+                <div className="play-progress-btn" style={{ left: `${leftPx}px` }}></div>
               </div>
-
               <div className="play-info-time">
-                <span className="time-now">00:00</span> / {durationPretty}
+                <span className="time-now">{timeProgress}</span> / {durationPretty}
               </div>
             </div>
           </div>
