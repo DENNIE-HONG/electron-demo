e62c240ac15a0f269e11da33c3ac2f1da1206aaa
hongluyan <hongluyan@baidu.com>
2019-09-18 15:45:31 +0800

    add  搜索单曲页播放功能

--- src/views/search/index.jsx
+++ src/views/search/index.jsx
@@ -8,6 +8,7 @@ import { parse } from 'querystringify';
 import { getSearch } from 'api/search';
 import LoadMore from 'coms/LoadMore';
 import BaseTable, { BaseTableColumn } from 'coms/BaseTable';
+import BaseButton from 'coms/BaseButton';
 import SearchAlbum from './SearchAlbum';
 import SearchPlaylist from './SearchPlaylist';
 import './search.scss';
@@ -32,6 +33,19 @@ class Search extends Component {
     });
   }
 
+  // 播放单曲
+  playSong = (song) => {
+    const { id } = song;
+    this.props.setMusic([song], id);
+  }
+
+  // 播放全部歌曲
+  playAllSongs = (songs) => {
+    const { id } = songs[0];
+    this.props.setMusic(songs, id);
+  }
+
+  // 切换tab
   handlerTabChange = (tag) => {
     switch (tag) {
       case 'albums':
@@ -67,7 +81,13 @@ class Search extends Component {
               isMountedFetch
               render={({ list }) => (
                 <section className="search-songs">
+                  <div className="search-songs-btns">
+                    <BaseButton icon="play" type="primary" onClick={this.playAllSongs.bind(this, list)}>播放全部</BaseButton>
+                  </div>
                   <BaseTable data={list} keyName="id">
+                    <BaseTableColumn width="40" onClick={(idx) => this.playSong(list[idx])}>
+                      <i className="iconfont icon-play search-songs-icon"></i>
+                    </BaseTableColumn>
                     <BaseTableColumn label="歌曲" prop="name" className="nav-link" onClick={(idx) => this.navLink(list[idx].id)}></BaseTableColumn>
                     <BaseTableColumn label="歌手" prop="artist" className="nav-link" onClick={(idx) => this.navLink(list[idx].artists[0].id, 'artist')}></BaseTableColumn>
                     <BaseTableColumn label="专辑" prop="albumName" className="nav-link" onClick={(idx) => this.navLink(list[idx].album.id, 'album')}></BaseTableColumn>
